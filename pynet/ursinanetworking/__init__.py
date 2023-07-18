2dd9b7385d6726524963e47d6688dd1299670bfc
Ivan Perzhinsky <94743980+xzripper@users.noreply.github.com>
2023-07-18 13:11:01 +0300

    Some information.
    Some information.

--- pynet/ursinanetworking/__init__.py
+++ pynet/ursinanetworking/__init__.py
@@ -1,1 +1,10 @@
+"""
+PyNet.
+
+by xzripper (Github) (UrsinaNetworking - K3#4869 (Discord) / Improved by xzripper (Github)).
+
+Last UrsinaNetworking version: 1.0.5.
+Last optimized version: 1.0.
+"""
+
 from .ursinanetworking import *
